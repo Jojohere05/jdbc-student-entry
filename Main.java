d4f5b2f76545af66af78bcec3a43032377d1ad8d
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-14 20:24:03 +0530

    while

--- Main.java
+++ Main.java
@@ -2,5 +2,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);  
+        while (true) { 
+            System.out.println("\n== Student Menu ==");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display All Students");
+            System.out.println("3. Search by PRN");
+            System.out.println("4. Update Student");
+            System.out.println("5. Delete Student");
+            System.out.println("6. Exit");
+            System.out.print("Choose: ");
+            int choice = sc.nextInt();
+            sc.nextLine();  // consume newline
     } 
+}
 }
