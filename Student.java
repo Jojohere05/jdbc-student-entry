8a78d1ed6f099de1cbba9b7fcaef062cce3f4e92
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-14 19:57:51 +0530

    setter dob

--- Student.java
+++ Student.java
@@ -14,4 +14,5 @@ public class Student {
     public String getDob() { return dob; }
     public double getMarks() { return marks; }
     public void setName(String name) { this.name = name; }
+    public void setDob(String dob) { this.dob = dob; }
 }
