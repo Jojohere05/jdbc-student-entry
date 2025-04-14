dee230276a0a14e491ff0dbf803157e417bfd415
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-14 20:07:49 +0530

    addstudent that throws sqlexception

--- StudentDAO.java
+++ StudentDAO.java
@@ -1,5 +1,8 @@
 import java.sql.*;
 import java.util.Scanner;
 public class StudentDAO {
-    
+    static Scanner sc = new Scanner(System.in);
+    public static void addStudent(Student student) throws SQLException {
+        
+    }
 }
