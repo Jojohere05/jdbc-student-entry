78106748cc013c2efe51dd78b99718d0d63a8b50
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-14 20:02:15 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 public class DBConnection {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 }
